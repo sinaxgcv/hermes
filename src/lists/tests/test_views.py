8e7970dfe2229ed11859efadd20ac58fe0efe46e
Michael Anckaert <michael.anckaert@sinax.be>
2022-02-01 15:37:42 +0100

    Add test coverage for List creation

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
 
+from lists.models import List
 from lists.tests.factories import ListFactory
 
 
@@ -42,3 +43,26 @@ class ListViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "lists/overview.html")
         self.assertCountEqual(response.context["lists"], [self.list3])
+
+    def test_list_create_unauthenticated(self):
+        response = self.client.get(reverse("lists-create"))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response, reverse("login") + "?next=" + reverse("lists-create")
+        )
+
+    def test_list_create(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse("lists-create"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "lists/form.html")
+
+        data = {"name": "Test List Creation", "description": ""}
+        response = self.client.post(reverse("lists-create"), data)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("lists-overview"))
+        created_list = List.objects.latest("id")
+        self.assertEqual(created_list.name, "Test List Creation")
