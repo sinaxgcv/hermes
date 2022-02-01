3055c357cd7171f86161b770cf54ab04e6426dad
Michael Anckaert <michael.anckaert@sinax.be>
2022-02-01 11:33:49 +0100

    Add List model

--- src/lists/models.py
+++ src/lists/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
 
+class List(models.Model):
+    name = models.CharField(unique=True, max_length=200)
+    description = models.TextField(blank=True, default="")
+
+    def __repr__(self):
+        return f"List ID {self.id} <{self.name}>"
+
+    def __str__(self):
+        return self.name
+
+
 class Subscriber(models.Model):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=200, blank=True, default="")
     last_name = models.CharField(max_length=200, blank=True, default="")
+
+    lists = models.ManyToManyField(List)
 
     def __repr__(self):
         return f"Subscriber ID {self.id} <{self.email}>"
--- src/lists/tests/test_models.py
+++ src/lists/tests/test_models.py
@@ -1,24 +1,31 @@
 from django.test import TestCase
 
-from lists.models import Subscriber
+from lists.models import List, Subscriber
+
+
+class ListTest(TestCase):
+    def test_repr(self):
+        my_list = List.objects.create(name="My First List")
+
+        expected_repr = f"List ID {my_list.id} <{my_list.name}>"
+        self.assertEqual(expected_repr, my_list.__repr__())
+
+    def test_str(self):
+        my_list = List.objects.create(name="My First List")
+
+        expected_str = my_list.name
+        self.assertEqual(expected_str, my_list.__str__())
 
 
 class SubscriberTest(TestCase):
-
     def test_repr(self):
         subscriber = Subscriber.objects.create(email="jack@example.com")
 
         expected_repr = f"Subscriber ID {subscriber.id} <{subscriber.email}>"
-        self.assertEqual(
-            expected_repr,
-            subscriber.__repr__()
-        )
+        self.assertEqual(expected_repr, subscriber.__repr__())
 
     def test_str(self):
         subscriber = Subscriber.objects.create(email="jack@example.com")
 
         expected_str = subscriber.email
-        self.assertEqual(
-            expected_str,
-            subscriber.__str__()
-        )
+        self.assertEqual(expected_str, subscriber.__str__())
