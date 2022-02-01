f67a803bc4be4cdde801fc4f0f6ca9a398d133aa
Michael Anckaert <michael.anckaert@sinax.be>
2022-02-01 11:17:24 +0100

    Fix settings for project module
    We use the convention where the 'project' module is under `src/project` instead of `src/hermes`. This commit fixes the Django project settings.

--- src/project/settings.py
+++ src/project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'lists',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'hermes.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'hermes.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
